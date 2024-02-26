6892f8ffaf1cead15b6ae5e2681fa80cea2df7fb
white8chimney <white8chimney@gmail.com>
2024-02-26 20:34:45 +0800

    Update read_quTag_binary_data.m

--- tools/read_quTag_binary_data.m
+++ tools/read_quTag_binary_data.m
@@ -5,16 +5,17 @@ filename = 'C:/Users/217-Entangle/Desktop/timestamps2.bin';
 fileinfo = dir(filename);
 filesize = fileinfo.bytes;
 headerSize = 40;
-eventNumber = (filesize - headerSize)/10
+nbrEvents = (filesize - headerSize)/10
 
 tic;
 
 fileID = fopen(filename);
 header = fread(fileID,headerSize,'uint8','ieee-le');
-data = fread(fileID,[5,eventNumber],'uint16','ieee-le');
+data = fread(fileID,[5,nbrEvents],'uint16','ieee-le');
 fclose(fileID);
 
-timeStamps = data(1,:).*(2^0) + data(2,:).*(2^16) + data(3,:).*(2^32) + data(4,:).*(2^48);
+timeStamps = data(1,:).*(2^0) + data(2,:).*(2^16) + ...
+             data(3,:).*(2^32) + data(4,:).*(2^48);
 channels = data(5,:);
 
 toc;

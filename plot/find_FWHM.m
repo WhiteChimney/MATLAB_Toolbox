15e06aa346b9833fd5a030dce3bc550ce2f5e528
white8chimney <white8chimney@gmail.com>
2024-03-21 00:00:54 +0800

    Update find_FWHM.m

--- plot/find_FWHM.m
+++ plot/find_FWHM.m
@@ -1,6 +1,10 @@
 function res = find_FWHM(x,y)
 % 求一个曲线的半高宽，只适用于单峰曲线
 
+y = abs(y);
+y = y - min(y);
+y = y./max(y);
+
 max_index = find(y==max(y));
 x_l = interp1(y(1:max_index),x(1:max_index),max(y)/2,'spline');
 x_r = interp1(y(max_index:end),x(max_index:end),max(y)/2,'spline');

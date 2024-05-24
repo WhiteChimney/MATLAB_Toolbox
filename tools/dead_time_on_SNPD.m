e08c8d72555777f7833e924b8f72551c87d461f5
white8chimney <white8chimney@gmail.com>
2024-05-24 11:30:03 +0800

    Update dead_time_on_SNPD.m

--- tools/dead_time_on_SNPD.m
+++ tools/dead_time_on_SNPD.m
@@ -1,4 +1,6 @@
 % 蒙卡计算死时间对超导连续探测模式下的效率影响
+% 本文件计算的是高平均光子数下长脉冲（～10 ns）使用超导探测时，死时间对探测结果的影响
+% 计算时假定死时间大于脉冲持续时间，因此每次脉冲最多造成一次响应
 
 clear;
 
@@ -6,17 +8,19 @@ t_pulse = 10.0;
                 % t_pulse < t_dead
 mu = 0.1;         % 每脉冲的平均光子数
 N_pulse = 1e8;  % 试验的脉冲数
-nbrBins = 100;  % bin size
+nbrBins = 100;  % 最终直方图的 bin size
 
 pN = exp(-mu).*mu.^(0:10)./factorial(0:10);
+                % 光子数分布，这里最多计算到 10 光子
 res = zeros(1,N_pulse);
 
 tic;
 parfor i = 1:N_pulse
-    n = find_N(rand(),pN);
+    n = find_N(rand(),pN);  % 此次实验的光子数
     if (n > 0)
-        t = rand(1,n);
+        t = rand(1,n);      % 这 n 个光子在时域上的分布情况
         res(i) = t_pulse.*min(t);
+                            % 探测器仅响应时域上第一个到达的脉冲
     end
 end
 toc;
